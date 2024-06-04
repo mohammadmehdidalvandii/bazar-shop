54d8426335bfe448871482d1f69248e24aac78bd
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-06-04 16:51:15 +0330

    create button menu

--- src/app/LoginRegister/page.jsx
+++ src/app/LoginRegister/page.jsx
@@ -1,12 +1,13 @@
 import Footer from '@/components/modules/Footer/Footer'
 import Navbar from '@/components/modules/Navbar/Navbar'
+import Menu from '@/components/template/loginRegister/Menu'
 import React from 'react'
 
 function page() {
   return (
     <>
         <Navbar/>
-
+        <Menu/>
         <Footer/>
     </>
   )
--- src/components/template/loginRegister/Menu.jsx
+++ src/components/template/loginRegister/Menu.jsx
@@ -1,1 +1,51 @@
-rfce
+"use client"
+import React, { useState } from 'react';
+import style from './Menu.module.css';
+
+
+function Menu() {
+    const [ChangeMenu , setChangeMenu] = useState('Login');
+
+    const handlerChangeMenu = (MenuID)=>{
+        setChangeMenu(MenuID);
+    }
+  return (
+    <section className={style.menu}>
+        <div className="containers">
+            <div className={style.menu_wrapper}>
+                <div className="row">
+                    <div className="col-12">
+                        <div>
+                        {ChangeMenu === "Login" ?(
+                                      <button className={style.menu_button}
+                                      onClick={()=>handlerChangeMenu("Register")}
+                                  >
+                                      ورود
+                                  </button>
+                        ):(
+                            <button className={style.menu_button}
+                            onClick={()=>handlerChangeMenu("Login")}
+                        >
+                            ثبت نام
+                        </button>
+                        )} 
+                        </div>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col-12">
+                    {ChangeMenu === "Login" &&(
+                        <h1>this is login</h1>
+                    )}
+                    {ChangeMenu === "Register" &&(
+                        <h1>this is Register</h1>
+                    )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Menu
