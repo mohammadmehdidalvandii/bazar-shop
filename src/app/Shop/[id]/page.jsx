26e47d76f59306dff8ee0a217b00d69a4868ad12
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-06-10 15:34:23 +0330

    fixed section productDetails  grid

--- src/app/Shop/[id]/page.jsx
+++ src/app/Shop/[id]/page.jsx
@@ -1,16 +1,23 @@
-import Breadcrumb from '@/components/modules/Breadcrumb/Breadcrumb'
-import Footer from '@/components/modules/Footer/Footer'
-import Navbar from '@/components/modules/Navbar/Navbar'
-import React from 'react'
+import Breadcrumb from "@/components/modules/Breadcrumb/Breadcrumb";
+import Footer from "@/components/modules/Footer/Footer";
+import Navbar from "@/components/modules/Navbar/Navbar";
+import React from "react";
 
 function page() {
   return (
     <>
-    <Navbar/>
-    <Breadcrumb text="جزئیات محصول"/>
-    <Footer/>
+      <Navbar />
+      <Breadcrumb text="جزئیات محصول" />
+      <div className="containers">
+        <div className="row mt-5">
+          <div className="col-12">
+            <div className="shop_Wrapper">this is shop wrapper</div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default page
+export default page;
